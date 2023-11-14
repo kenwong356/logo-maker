97f8be289c02672284ecabab2e9dde0fedec2ec6
Ken <kanney356@gmail.com>
2023-11-13 23:02:45 -0800

    Fixed index.js

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle, Square, Triangle} = require("./lib/shapes");
-
+const { Circle, Triangle, Square } = require('./lib/shapes.js');
 
 const shapeConstructor = {
-    circle: Circle,
-    square: Square,
-    triangle: Triangle
-  };
-  
-  async function generateSvg() {
-    try {
-      const answers = await inquirer.prompt([
+  circle: Circle,
+  triangle: Triangle,
+  square: Square
+};
+
+async function generateSvg() {
+  try {
+    const answers = await inquirer.prompt([
         {
             type: 'input',
             name: 'text',
-            message: 'Please enter any three characters for logo'
-        },
-        {
+            message: 'Please enter any three characters for the logo',
+          },
+          {
             type: 'input',
             name: 'textColor',
-            message: 'Please enter a color keyword or hexadecimal color code for your texts color'
-        },
-
-        {
+            message: 'Please enter a color keyword or hexadecimal color code for your texts color',
+          },
+          {
             type: 'list',
             name: 'shape',
             message: 'What shape would you like your logo to be?',
-            choices: ['Circle', 'Triangle', 'Square']
-        },
-        {
+            choices: ['circle', 'triangle', 'square']
+          },
+          {
             type: 'input',
             name: 'shapeColor',
             message: 'Please enter a color keyword or hexadecimal for your shapes color',
-        },
-    ]);
-    
-      } catch (error) {
-        console.error(error);
-      }
+          }
+        ]);
+
+    const { text: userText, textColor: userTextColor, shape: userShape, shapeColor: userShapeColor } = answers;
+
+    console.log(userShape);
+
+    let svgContent = '';
+
+    if (shapeConstructor[userShape]) {
+      const userShapeInstance = new shapeConstructor[userShape](userText, userTextColor, userShape, userShapeColor);
+      svgContent = userShapeInstance.generateSvg();
     }
-    
-    generateSvg();
+
+    fs.writeFileSync('./example.svg', svgContent, 'utf-8');
+    console.log('SVG generated!');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+generateSvg();
